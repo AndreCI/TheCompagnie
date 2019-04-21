14129440b9cacf3f7eafa5f71f6196a4a8c7253f
AndreCI <andre.cibils@gmail.com>
2019-04-21 21:02:27 +0200

    First Combat Engine! Card drag and drop + basic unit info & interaction with cards

---
+++ Assets/Scripts/Cards/Card.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Card : Draggable
+{
+    public override void OnEndDrag(PointerEventData pointerEventData)
+    {
+        base.OnEndDrag(pointerEventData);
+        Debug.Log("Toast");
+        Compagnion target = this.transform.parent.transform.parent.GetComponent<Compagnion>();
+        if (target != null)
+        {
+            target.TakeDamage(6);
+        }
+    }
+}
---
+++ Assets/Scripts/Characteres/Compagnions/Compagnion.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compagnion : MonoBehaviour
+{
+    public int currentHealth;
+    public int maxHealth;
+
+    private Text jack;
+
+    void Start()
+    {
+        jack = this.GetComponentInChildren<Text>();
+        jack.text = maxHealth.ToString() + "/" + maxHealth.ToString();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        jack.text = currentHealth.ToString() + "/" + maxHealth.ToString();
+        if (currentHealth <= 0)
+        {
+            Debug.Log("Dead");
+        }
+    }
+}
